7cb0fff8cfa215667398ee86a85829d9f89b9837
FockeWulf44 <cruel.programmer@gmail.com>
2024-01-27 08:35:47 -0800

    Creating folder for everyone

--- Test1.py
+++ Test1.py
@@ -1,7 +1,13 @@
-print("Hello World")
+from math import sqrt
+
 x = 1
 y = 2
 
-print(x/y)
+numbers = [1, 2, 3, 4, 5, 6, 7]
 
-print('Please work...')
+
+def print_values(**somevalues):
+    print(somevalues)
+
+
+print_values(x=2, y=2, z=3)
