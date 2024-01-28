f130fe8fa707ebcc693c4ee2978c39d4b821a99e
adrianod204 <adriano.dicostanzo@gmail.com>
2024-01-28 14:57:43 -0800

    This is my code for the Palindrome algorithm

---
+++ AdrianoD/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(x: int) -> bool:
+        if x == abs(x):
+            return True
+
+        i = 0
+        j = len(str(x)) - 1
+        s = str(x)
+
+        while i < len(s):
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+
+        if i == j:
+            return True
+
+    print(isPalindrome(121))
