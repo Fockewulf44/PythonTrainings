078abe6c2ecff7075027d2f4b20b0870544419fd
adrianod204 <adriano.dicostanzo@gmail.com>
2024-01-28 15:42:40 -0800

    Tell me what's wrong with this code.

--- AdrianoD/Algorithms/Palindrome Solution.py
+++ AdrianoD/Algorithms/Palindrome Solution.py
@@ -1,19 +1,16 @@
-class Solution:
-    def isPalindrome(x: int) -> bool:
-        if x == abs(x):
-            return True
+def isPalindrome(x: int) -> bool:
+    if x == abs(x):
+        return True
 
-        i = 0
-        j = len(str(x)) - 1
-        s = str(x)
+    i = 0
+    j = len(str(x)) - 1
+    s = str(x)
 
-        while i < len(s):
-            if s[i] != s[j]:
-                return False
-            i += 1
-            j -= 1
+    while i < len(s):
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
 
-        if i == j:
-            return True
-
-    print(isPalindrome(121))
+    if i == j:
+        return True
