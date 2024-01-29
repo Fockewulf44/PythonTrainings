55a6cd1701f2291c48bbad2c7c6dc96f8162d89c
FockeWulf44 <cruel.programmer@gmail.com>
2024-01-28 16:07:47 -0800

    Added comments and did minor refactoring

--- AdrianoD/Algorithms/Palindrome Solution.py
+++ AdrianoD/Algorithms/Palindrome Solution.py
@@ -1,16 +1,19 @@
 def isPalindrome(x: int) -> bool:
+    # Why does it return True? It should return false ONLY
+    # if the number is negative
     if x == abs(x):
         return True
 
-    i = 0
-    j = len(str(x)) - 1
+    i = 0    
     s = str(x)
+    j = len(s) - 1
 
     while i < len(s):
         if s[i] != s[j]:
             return False
         i += 1
         j -= 1
-
+    # This IF must be inside While loop
+    # But currently it is not inside
     if i == j:
         return True
