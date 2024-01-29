a086589761f9963c90a6c96b6b5b109b6c9f0f3f
FockeWulf44 <cruel.programmer@gmail.com>
2024-01-28 18:18:54 -0800

    Review changes, added some comments

--- AdrianoD/Algorithms/Palindrome Solution.py
+++ AdrianoD/Algorithms/Palindrome Solution.py
@@ -1,5 +1,6 @@
 # The algorithm passed all testcases except x = 0, it returns None instead of True.  Why is this?
-def isPalindrome(x: int) -> bool:
+# Answer -> your function doesn't have any default. So, it should return False by dfault
+def isPalindrome(x: int):
     if x != abs(x):
         return False
 
@@ -14,4 +15,9 @@ def isPalindrome(x: int) -> bool:
         j -= 1
         if i == j:
             return True
+
+    # Here your function should return False in case if it doesn't return true
+    # Try also debug the code. Put a breakpoint and debug and see how it goes
  
+ 
+print(isPalindrome(0))
