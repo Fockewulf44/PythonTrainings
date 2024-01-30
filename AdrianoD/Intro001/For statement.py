82843a2e2fed0ab0351494329a1de792ab6acbda
adrianod204 <adriano.dicostanzo@gmail.com>
2024-01-30 08:18:07 -0800

    Just added an example of a basic 'for' statement.

---
+++ AdrianoD/Intro001/For statement.py
@@ -0,0 +1,11 @@
+# basic 'for' statement
+
+colors = ['red', 'green', 'yellow']
+
+for color in colors:
+    print(color)
+
+# 'color' after 'for' represents the index of the list. This function means that for every index of the list colors, 
+#  we will print its corresponding value. The name 'color' is arbitrary.
+
+
