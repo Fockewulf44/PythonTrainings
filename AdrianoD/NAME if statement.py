129cf086793ed794262431d6ab4b9617d6e494ec
adrianod204 <adriano.dicostanzo@gmail.com>
2024-02-17 10:00:58 -0800

    Simple IF statement.

---
+++ AdrianoD/NAME if statement.py
@@ -0,0 +1,10 @@
+
+name = input('What is your name? ')
+
+if 'A'<= name[0] <= 'G':
+    print('Your name is from A-G')
+elif 'H' <= name[0] <= 'P':
+    print('Your name is from H-P')
+else:
+    print('Your name is from Q-Z')
+
