915fdd5f5162b30b979074f2056c0c67bbf26b5d
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-02 07:48:01 -0800

    March1st

---
+++ Saif/Chapter1.py
@@ -0,0 +1,15 @@
+# If you are familiar with other computer languages, you may be used to terminating every line with asemicolon. 
+# There is no need to do so in Python. A line is a line, more or less. You may add a semicolon if you like, but 
+# it won’t have any effect (unless more code follows on the same line), and it is not a common thing to do.
+
+# print("Hello, world!")
+
+#  help() for python modules, keywords, symbols, or topics. 
+
+# Algorithms consist of a series of instructions to be followed in order. Some of the instructions may be done directly (“take some SPAM”), while some require
+#  some deliberation (“If a particularly spicy SPAM is desired”), and others must be repeated several times (“Check every 10 minutes.”)
+
+# Recipes and algorithms consist of ingredients (objects, things) and instructions (statements). In this
+# example, SPAM and eggs are the ingredients, while the instructions consist of adding SPAM, cooking for a
+# given length of time, and so on.
+
---
+++ Saif/Tuple.py
@@ -0,0 +1,9 @@
+x = ("USA", "France", "Italy", "Pakistan", "India")
+y = (100, 200, 300, 400, 500, 600)
+z = x+y
+
+def abc(a,b,c):
+    xyz = a+b+c
+    return xyz
+    
+print(abc(x,y,z))
