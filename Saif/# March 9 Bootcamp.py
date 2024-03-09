1a5babb71702bbf1794fa1b7bd442a4a213e6aff
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-09 08:42:43 -0800

    march 9 class1

---
+++ Saif/# March 9 Bootcamp.py
@@ -0,0 +1,24 @@
+# March 9 Bootcamp
+
+# website = "https://google.com"
+
+# website [-3:] = "org"
+
+# print(website)
+# result = TypeError: 'str' object does not support item assignment
+
+# format = "Hello, %s. %s enough for ya"
+# values = ("world", "Hot")
+# print(format % values)
+# # returns: "Hello, world. Hot enough for ya" i.e. %s is used whenever you have key words (world, Hot)
+
+
+# Page 46
+
+# result = "{}, {}, and {}".format('first','second','third')
+# print(result)
+# .format is applied in front of a string to replace values in curly brackets with those in .format, moving in order
+
+ from math import pi
+# "{name} is approximately {value:.2f}.".format(value=pi, name=)
+
