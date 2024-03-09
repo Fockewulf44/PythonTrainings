fd60e2c59a09f5868b58750d4dbddb0881e33728
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-09 11:04:17 -0800

    new find distance example

---
+++ his example find distance.py
@@ -0,0 +1,10 @@
+# HIS Distance example: Find the distance between the two furthest apart values in a list.
+
+List = [9, 6, 3, 5, 15, 4, 7]
+highest = 0
+lowest = 1000
+for i in list:
+    if i > highest:
+        
+print(sortedlist)
+
---
+++ his-example-2.py
@@ -0,0 +1,8 @@
+list = [5, 10, 1, 4, 100, 60, 2000, 3, 2]
+biggestnumber = 0
+for num in list:
+    if num > biggestnumber:
+        biggestnumber = num
+print(biggestnumber)
+
+# useful to define 0 or -1 when trying to sort of max or min
