55ff605f38719032f190d67631feac7781875100
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-09 08:52:06 -0800

    march 9 class

--- Saif/# March 9 Bootcamp.py
+++ Saif/# March 9 Bootcamp.py
@@ -19,6 +19,15 @@
 # print(result)
 # .format is applied in front of a string to replace values in curly brackets with those in .format, moving in order
 
- from math import pi
-# "{name} is approximately {value:.2f}.".format(value=pi, name=)
+# f-string = formatted strings
+# import math
+# print(
+#     f'The value of pie is approximately {math.pi:.3f}.'
+#     )
+# # returns The value of pie is approximately 3.142.
 
+# animals = 'eels'
+# animals2 = 'cats'
+# print(f'My hovercraft is full of {animals} and {animals2}.')
+# # returns "My hovercraft is full of eels and cats."
+# if you remove f early, it'll return My hovercraft is full of {animals} and {animals2}.
