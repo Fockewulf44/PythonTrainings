ff56374b04a52cd36ec986fad696f19f19c07093
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-16 15:09:11 -0700

    mar16

--- Saif/Examples/Hackinscience - Playing with Anagrams.py
+++ Saif/Examples/Hackinscience - Playing with Anagrams.py
@@ -1,18 +1,32 @@
 #Hackinscience - Playing with Anagrams
 # https://www.hackinscience.org/exercises/is_anagram
 
-"funeral"
-"real fun"
+"character"
+"limit"
+# 1st iteration
+# Letter f; replace f with "" --> "real un"
 
-def is_anagram(str1: str, str2: str) -> bool:
+# 2nd iteration
+# Letter u; replace u with "" --> "real n"
+
+# def is_anagram(str1: str, str2: str) -> bool:
     
-    for c in str1:
-        str2 = str2.replace(c,"")
+#     for character in str1:
+#         str2 = str2.replace(character,"")
     
-    if str2.replace(" ","") == "":
-        return True
-    else:
-        return False
+#     if str2.replace(" ","") == "":
+#         return True
+#     else:
+#         return False
     
-result = is_anagram("funeral", "real fun")
-print(result)
+# result = is_anagram("character", "limit")
+# print(result)
+
+def is_anaram(str1: str, str2: str) -> bool:
+    
+    str1_dict=[]
+    for character in str1:
+        ind1 = str2.find(character)
+        str2 = str2(0:ind1) + str2(0:ind1+1)
+        
+        
