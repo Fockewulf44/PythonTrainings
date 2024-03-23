8b5f203e97892de864155024baa09fb955d04bed
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-23 11:10:29 -0700

    mar

---
+++ Saif/Examples/Exceptions.py
@@ -0,0 +1,52 @@
+
+# Exceptions
+# try/except/finally
+
+# try:
+#     result = 2/0
+# except ZeroDivisionError:
+#     print("Division by zero")
+# except Exception as e:
+#     print("Generic exception")
+# finally:
+#     pass
+
+
+# try:
+#     s = "hello world"
+#     result = s/2
+# except ZeroDivisionError:
+#     print("Division by zero")
+# except Exception as e:
+#     print(e)
+# finally:
+#     pass
+
+
+# d1 = {1:"San Francisco", 2:"Karachi", 3:"Baku"}
+
+# try:
+#     print(f"Value of key 1 is {d1[1]}")
+#     print(f"Value of key 5 is {d1[5]}")
+# except ZeroDivisionError:
+#     print("Division by zero")
+# except Exception as e:
+#     print(e)
+#     print(e.args)
+#     print(type(e))
+#     print(f"Type error i {type(e)}, and the message is {e.args}")
+# finally:
+#     pass
+    
+    
+bignumber = 200
+list1 = [4,11,10,0,8]
+
+try:
+    for number in list1:
+        print(f"Dividing {bignumber} by {number}")
+        print(bignumber/number)
+except Exception as e:
+    print(e.args)
+    
+print("Calculcations are done")
