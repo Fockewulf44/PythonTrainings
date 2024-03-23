e54bd768b6931fa7c213d69c9c3a27bb35ece6c3
adrianod204 <adriano.dicostanzo@gmail.com>
2024-03-23 08:53:32 -0700

    Changed file location of "2 letter pairs"

--- AdrianoD/Intro004/string formatting.py
+++ AdrianoD/Intro004/string formatting.py
@@ -8,13 +8,13 @@ print(result)
 
 # f strings
 import math
+
 print(f"The value of pi is approximately {math.pi:.3f}.")
 
 from math import e
+
 print(f"Euler's constant is roughly {e}")
 
 animals = "eels"
 animals2 = "cats"
 print(f"My hovercraft is full of {animals} and {animals2}.")
-
-
