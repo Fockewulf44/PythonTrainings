d7eec3b8d1c451503080b5882d6ed1202151d7e3
nataly2202 <nataly220285@gmail.com>
2024-03-23 10:27:35 -0700

    homework 6&7

---
+++ Nataly/Chapter 6/chapter6.py
@@ -0,0 +1,12 @@
+class car:
+    def _init_(self, color, make):
+        self.name = "toyota"
+        self.color = "red"
+        self.make = "corolla"
+
+class cellphone:
+    def __init__(self, brand)
+        self.name = "my phone"
+        self.brand ="samsung"
+
+
