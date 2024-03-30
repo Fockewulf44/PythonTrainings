95e1c97e057d15dd7dc20445f5c561b187e38eec
Saif Akhtar <saifakhtar408@gmail.com>
2024-03-30 10:35:39 -0700

    mar30

--- Saif/Classes/March30.py
+++ Saif/Classes/March30.py
@@ -1,1 +1,41 @@
 # March30
+# Write a program that takes a list of numbers and makes a new list of only the first and last elements of the given list. 
+# For practice, write this code inside a function and inside use try/except
+
+try:
+    list = [5, 10, 15, 20, 25]
+
+
+    first_number=list[0]
+    print(first_number)
+    
+    last_index=len(list) - 1
+    print(last_index)
+    
+    last_number=list[last_index]
+    print(last_number)
+    
+    # calc_lastindex = len.list[-1]
+    # print(calc_lastindex)
+    newlist = []
+    newlist.append(first_number)
+    newlist.append(last_number)
+    
+    print(newlist)
+
+except Exception as e:
+    print(e.args)
+
+# last_number=list[4]
+
+
+
+# def first_number():
+    
+    
+# def last_number():
+    
+    
+# new_list = append first_number.last_number
+
+# print(new_list)
--- Saif/Homework/HW7.py
+++ Saif/Homework/HW7.py
@@ -1,1 +1,68 @@
 # HW7
+# Class model
+class DailyForecast:
+    max_temperature=0
+    min_temperature=0
+    forecast_date=None
+
+# Class model
+class HourlyForecast:
+    datetime=None
+    temperature=0
+    iconphrase=""
+    
+class WeatherCondition:
+    
+    daily_forecast_response=""
+    hourly_forecast_response=""
+    
+    #DailyForecast
+    daily_forecast:DailyForecast
+    
+    # list of HourlyForecast
+    hourly_forecast = []
+    
+    def ExampleMethod(self):
+        HourlyForecast1 = HourlyForecast()
+        HourlyForecast1.temperature=100
+        HourlyForecast1.iconphrase="Pretty foggy"
+        
+        self.hourly_forecast.append(HourlyForecast1)
+        
+        return None
+    
+    def __init__(self) -> None:
+        # call here ONLY getDailyForecast
+        # and getHourlyForecast
+        pass
+    
+    def getDailyForecast():
+        # Call the DailyForecastAPI 
+        # and save result in self.daily_forecast_response
+        pass
+    
+    def getHourlyForecast():
+        # Call the HourlyForecastAPI 
+        # and save result in self.hourly_forecast_response
+        pass
+    
+    def ParseDailyForecastResponse():
+        # Parse self.daily_forecast_response 
+        # and save it in a new variable daily_Forecast
+        pass
+        
+    def ParseHourlyForecastResponse():
+        # Parse self.hourly_forecast_response 
+        # and save it in a new variable hourly_Forecast
+        pass
+        
+        for i in range(100):
+            hourly_forecast.append(HourlyForecast())
+            
+
+    def PrintTodaysDailyTemp():
+        print()
+        
+    def PrintTodaysHourlyTemp():
+        print()
+        
