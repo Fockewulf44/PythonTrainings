4786c5eaba403841d5479eeb006380f490b977d1
Omair Khan <omairkhan@berkeley.edu>
2024-05-05 01:56:01 -0700

    my first homework

--- Omair_Khan/test1.py
+++ Omair_Khan/test1.py
@@ -6,4 +6,17 @@ print(m.sqrt(x))
 problem = 5000 * 3
 print (problem)
 
+value = input("What is your first name?: ")
+print ("Congrats on completing your first coding homework " + value + "!")
+
+test = 9//3
+print (test)
+
+print (abs(-10))
+
+print (round(97/7))
+
+
+
+
 
