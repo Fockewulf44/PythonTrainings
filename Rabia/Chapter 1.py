05b1be32119fbf1925757a3f5570d84753199188
akan9 <rabiaakan91@gmail.com>
2024-05-06 11:49:41 -0700

    my first commit

---
+++ Rabia/Chapter 1.py
@@ -0,0 +1,43 @@
+# Numbers and Expressions
+
+from math import sqrt
+
+
+93883 + 8202472
+
+876 * 9274
+
+10 / 3
+
+10 // 3               #integer division - positive numbers are rounded up
+
+-10 // 3                                #negative numbers are rounded down
+
+10 % 3                                   # remainder
+
+10 ** 3                               #exponentation
+
+# Variables and Statements
+
+x1 = 5  #variable
+x2 = 4
+print (x1+x2)  #statement
+x1 * 5
+
+#Input - this was a litte confusing. Skipping for now.
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+#Functions - examples
+
+pow(10, 3)      #exponentation function
+
+round(10/3)     #rounds to nearest integer
+
+abs(-20)     #gives absolute value
+
+sqrt(25)     #square root function
+
+#Continue at Strings section in Chapter 1
+
