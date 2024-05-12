fb0474fb27fd4c52d4b700853f14f034e7cf9335
Omair Khan <omairkhan@berkeley.edu>
2024-05-12 09:44:56 -0700

    Homework_002

---
+++ Omair_Khan/Homework_2/Intro_002.py
@@ -0,0 +1,12 @@
+hello = ["January", "March", "February", "November", "June", "July"]
+hello.sort()
+print(hello)
+
+team = [1,8,4,5,3,2,1]
+team.insert(4, "twenty one")
+print(team)
+
+a = [100, 200, 300, 400, 500]
+first = a[0]
+last = a[4]
+print(first, last)
